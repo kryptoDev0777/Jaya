b0c0ae9cff24d81fe81f77662540efddd92692b8
Richard382 <73242220+Richard382@users.noreply.github.com>
2021-03-26 18:13:48 +1000

    Update search.js

--- cypress/page-objects/search.js
+++ cypress/page-objects/search.js
@@ -5,6 +5,7 @@ module.exports = {
             toRentTravelTime: '[data-testid=header-mobile-menu] > nav > ul > li:nth-child(3) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)',
             housePrices: '[data-testid=header-primarynav-mn-prices] > [data-testid=header-primarynav-accordion] > [data-testid=header-primarynav-link]',
             signIn: '.css-1oboy4t-LogoWrapper > .css-mein0i-ProfileMenuWrapper > [data-testid=header-profile] > [data-is-logged-in="false"] > [data-testid=header-profile-sign-in]', 
+            alertandSearch: '[data-testid=header-profilemenu-alerts-searches]',
         },
         sModal: {
             location: '#search-input-location',
@@ -24,5 +25,7 @@ module.exports = {
         searchResultCount: '[data-testid=total-results]',
         acceptCookie: '.ui-cookie-consent-choose__buttons > .ui-button-primary',
         alertFrequency: '.js-myaccount-alert-frequency',
+        searchResult: '#ui-id-4',
+        tab_to_rent: '#tab-residential-to-rent > div:nth-child(2) > form > div:nth-child(5) > div > a:nth-child(1)', 
     }
 } 
